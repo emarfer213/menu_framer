2d1b1b176822a156debc32434c7674fd3793f349
emarfer213 <emiliomartinf2002@gmail.com>
2026-01-21 19:51:36 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/menu_framer/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.menu_framer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/pantallas/home.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+class homeScreen extends StatefulWidget {
+  const homeScreen({super.key});
+
+  @override
+  State<homeScreen> createState() => _homeScreenState();
+}
+
+class _homeScreenState extends State<homeScreen> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: SafeArea(
+          child: Container(
+            color: Colors.grey[300],
+            child: Column(
+              spacing: 50,
+              children: [
+                Center(
+                  child: SizedBox(
+                    width: 250,
+                    height: 250,
+                    child: Image.asset("assets/cocina.png"),
+                  ),
+                ),
+                camposTextHomeScreen(),
+                ElevatedButton.icon(
+                  onPressed: () {
+                    //Navigator.pushNamed(context, '/terms_conditions.dart');
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color.fromRGBO(239, 56, 103, 1),
+                  ),
+                  label: Text("GO!!", style: TextStyle(color: Colors.black),),
+                )
+              ],
+            ),
+          )
+      ),
+    );
+  }
+}
+
+class camposTextHomeScreen extends StatelessWidget {
+  const camposTextHomeScreen({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      spacing: 10,
+      children: [
+        Center(
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 50),
+            child: TextFormField(
+              decoration: InputDecoration(
+                  hintText: "First Name",
+                  border: OutlineInputBorder()
+              ),
+            ),
+          ),
+        ),
+        Center(
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 50),
+            child: TextFormField(
+              decoration: InputDecoration(
+                  hintText: "Second Name",
+                  border: OutlineInputBorder()
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
